77d69e7294a2367bcb846dfb85d25632d20a7da5
sansdieu <jm.saminada@gmail.com>
2020-01-12 17:46:31 +0100

    12/01/2020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { SearchComponent } from './components/search/search.component';
-import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -16,11 +13,7 @@ import { TableModule } from 'primeng/table';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
     FormsModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    TableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
